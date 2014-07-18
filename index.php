a50aeaffc388c047d9c9c0dd5b475964fcf86de1
benlaod <benlaod@gmail.com>
2014-07-18 05:52:07 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<DOCTYPE!>
+	<html>
+		<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+			<title>
+				Gamer For Ever
+			</title>
+			<link rel="stylesheet" type="text/css" href="style.css">
+
+		</head>
+		<body>
+			<header>
+				<div class="logo"><h1>Gamer For Ever</h1></div>
+				<nav class="menu-ext">
+					<ul>
+						<li><a href=""><img src="stockimg/logoyt.png" alt="youtube" height="50" width="50" /></a></li>
+						<li><a href=""><img src="stockimg/logofb.png" alt="facebook" height="50" width="50"/></a></li>
+					</ul>
+				</nav>
+				<nav class="menu">
+					<ul>
+						<li><a href="index.php">Accueil</a></li>
+						<li><a href="news.php">News</a></li>
+						<li><a href="">Médias</a></li>
+					</ul>
+				</nav>
+			</header>
+			<section>
+				
+			</section>
+			<footer>
+				<p>Site par <a href="http://benjaminbrasseur.fr.cr">Benjamin Brasseur</a></p>
+			</footer>
+		</body>
+	</html>
